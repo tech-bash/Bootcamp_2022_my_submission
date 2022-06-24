3c356c0cfa5fadf4195f81177e43b7952f11e021
Bhavy Airi <airiragahv@gmail.com>
2022-06-24 15:18:55 +0530

    changing the shell script

---
+++ Week_1/shell-script/array.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+array=("man" "bear" "kid" "dog" "sheep")
+for item in "${array[@]}"
+do
+        echo $item
+done
---
+++ Week_1/shell-script/dirs.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+for (( i=1; i < 6; i++ ))
+do
+	mkdir -p root/dir${i}
+	for (( j=1; j < 5; j++ ))
+	do
+		touch root/dir${i}/file${j}
+		for (( k=1; k <= j; k++ ))
+		do
+			echo ${j} >> root/dir${i}/file${j}
+		done
+	done
+done
